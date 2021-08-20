99c62f62a58e661c0c09eaede877784b9803267e
onewhich <which_1987@163.com>
2021-08-20 12:19:12 +0200

    update API test

--- src/API/call_MoREST_from_Fortran/test_use_module.f90
+++ src/API/call_MoREST_from_Fortran/test_use_module.f90
@@ -8,7 +8,9 @@ program call_morest
   simulation_temperature = 798
   current_md_step = 1
   allocate(md_force(3,4))
+  allocate(coordinate(3,4))
   md_force_shape = shape(md_force) !(/3,4/)
+  coordinate_shape = shape(coordinate)
 !  md_force = reshape((/1, 2, 3, 1, 3, 2, 3, 1, 2, 3, 2, 1/), md_force_shape)
 
   call random_seed()
@@ -22,14 +24,16 @@ program call_morest
 
   call random_number(potential_energy)
   call random_number(md_force)
+  call random_number(coordinate)
 
-!  write(*,*) md_force
+  write(*,*) md_force
 !  write(*,*) current_md_step
 
-  call call_morest_its(if_initial, simulation_temperature, potential_energy*1000,&
-                       current_md_step, md_force, md_force_shape)
+  call call_morest_bias_sampling(if_initial, simulation_temperature, potential_energy,&
+                       current_md_step, md_force, md_force_shape,&
+                       coordinate, coordinate_shape)
 
-!  write(*,*) md_force
+  write(*,*) md_force
 !  write(*,*) current_md_step
 
   current_md_step = current_md_step + 1
