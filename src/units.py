4b607c177ee58db2467a68dea7cb9d938842b3eb
onewhich <which_1987@163.com>
2021-10-03 09:20:24 +0200

    finish md nve

---
+++ src/units.py
@@ -0,0 +1,13 @@
+#mass
+kg = 694461.548335367        # eV AA^-2 ps^2
+Dalton = 1.1531805312624011e-21    # eV AA^-2 ps^2
+
+# time
+s = 1e12                 # ps
+
+# length
+m = 1e10                 # AA
+nm = 10                 # AA
+
+# velocity
+m_s = 1e-2               # AA/ps
