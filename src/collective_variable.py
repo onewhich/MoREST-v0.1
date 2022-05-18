fc7694caaddc24cd520fe90662df63ce07af9ba7
onewhich <which_1987@163.com>
2022-05-18 09:42:56 +0100

    add collective variable distance

--- src/collective_variable.py
+++ src/collective_variable.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 import numpy as np
 
 class collective_variable:
@@ -13,7 +14,7 @@ class collective_variable:
             if type(self.CVs_list) != type([]): # type(list) returns type
                 raise Exception('Please specify the CVs in a list (CVs_list).')
 
-    def check_CVs_one(self, system, output=False):
+    def generate_checks(self, system, output=False):
         checks = []
         for i_CV in self.CVs_list:
             if i_CV[0] == 'central_R_one':
@@ -21,7 +22,17 @@ class collective_variable:
                     checks.append(self.central_R_one(system, i_CV[1], i_CV[2], i_CV[3]))
                 else:
                     checks.append(self.central_R_one(system, i_CV[1], i_CV[2], i_CV[3], i_CV[4]))
-        checks = np.array(checks)
+            if i_CV[0] == 'central_R_all':
+                if i_CV[3] == 'all':
+                    checks.append(self.central_R_all(system, i_CV[1], i_CV[2], i_CV[3]))
+                else:
+                    checks.append(self.central_R_all(system, i_CV[1], i_CV[2], i_CV[3], i_CV[4]))
+            if i_CV[0] == 'distance':
+                checks.append(self.central_R_all(system, i_CV[1], i_CV[2], i_CV[3]))
+        return np.array(checks)
+
+    def check_CVs_one(self, system, output=False):
+        checks = self.generate_checks(system, output)
         if output:
             pass
         else:
@@ -31,14 +42,7 @@ class collective_variable:
                 return False
 
     def check_CVs_all(self, system, output=False):
-        checks = []
-        for i_CV in self.CVs_list:
-            if i_CV[0] == 'central_R_one':
-                if i_CV[3] == 'all':
-                    checks.append(self.central_R_one(system, i_CV[1], i_CV[2], i_CV[3]))
-                else:
-                    checks.append(self.central_R_one(system, i_CV[1], i_CV[2], i_CV[3], i_CV[4]))
-        checks = np.array(checks)
+        checks = self.generate_checks(system, output)
         if output:
             pass
         else:
@@ -64,6 +68,43 @@ class collective_variable:
         elif checker == 1:
             check_result = np.where(central_R > cutoff, 1, 0)
             if np.sum(check_result) > 0:
+                return True
+            else:
+                return False
+
+    def central_R_all(self, system, checker, cutoff, N_check, atom_list=None):
+        coordinates = system.get_positions()
+        if N_check == 'all':
+            central_R = np.linalg.norm(coordinates, axis=1)
+        else:
+            atom_list = np.array(atom_list)
+            atom_list -= 1
+            central_R = np.linalg.norm(coordinates[atom_list], axis=1)
+        if checker == -1:
+            check_result = np.where(central_R < cutoff, 1, 0)
+            if np.sum(check_result) == len(central_R):
+                return True
+            else:
+                return False
+        elif checker == 1:
+            check_result = np.where(central_R > cutoff, 1, 0)
+            if np.sum(check_result) == len(central_R):
+                return True
+            else:
+                return False
+
+    def distance(self, system, checker, cutoff, atom1, atom2):
+        coordinates = system.get_positions()
+        atom1_coordinate = coordinates[atom1-1]
+        atom2_coordinate = coordinates[atom2-1]
+        R_1_2 = np.linalg.norm(atom1_coordinate - atom2_coordinate)
+        if checker == -1:
+            if R_1_2 < cutoff:
+                return True
+            else:
+                return False
+        elif checker == 1:
+            if R_1_2 > cutoff:
                 return True
             else:
                 return False
