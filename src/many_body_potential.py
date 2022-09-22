3e9881622c94bcc43cc4f8e883e021e77e667c37
onewhich <which_1987@163.com>
2022-09-22 06:47:53 +0200

    Update many_body_potential.py

--- src/many_body_potential.py
+++ src/many_body_potential.py
@@ -397,7 +397,7 @@ class molpro_calculator:
             #    return path, elements, xyz, energy
 
             if if_get_force:
-                return energy * units.Hartree, np.array(force) * (units.Hartree/units.Bohr)
+                return energy * units.Hartree, np.array(force) * (units.Hartree/units.Angstrom)
             else:
                 return energy * units.Hartree
 
