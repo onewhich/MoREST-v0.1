3436969016932a2785c6ef778b9698be71b52463
onewhich <which_1987@163.com>
2023-06-25 01:13:00 +0200

    Update structure_optimizing.py

--- src/structure_optimizing.py
+++ src/structure_optimizing.py
@@ -166,6 +166,12 @@ class gradient_descent(initialize_optimizing):
             # rho(k) = 1/(y(k)^T @ s(k))
             rho_k = np.array([1/(y_k[i] @ s_k[i]) for i in range(self.n_atom)])
             # H(k+1) = (I - rho(k) s(k) y(k).T) H(k) (I - rho(k) y(k) s(k).T) + rho(k) s(k) s(k).T
+            for i in range(self.n_atom):
+                print(np.identity(3) - rho_k[i] @ np.outer(s_k[i], y_k[i]))
+                print(self.H_k[i])
+                print(np.identity(3) - rho_k[i] @ np.outer(y_k[i], s_k[i]))
+                print(rho_k[i] @ np.outer(s_k[i], s_k[i]))
+
             next_H = [(np.identity(3) - rho_k[i] @ np.outer(s_k[i], y_k[i])) @ self.H_k[i] @ (np.identity(3) - rho_k[i] @ np.outer(y_k[i], s_k[i])) + \
               rho_k[i] @ np.outer(s_k[i], s_k[i]) for i in range(self.n_atom)]
             
