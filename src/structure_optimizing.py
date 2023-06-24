3f0f08d503da7e0a835aa12723fc2eaf2af30976
onewhich <which_1987@163.com>
2023-06-25 01:02:36 +0200

    Update structure_optimizing.py

--- src/structure_optimizing.py
+++ src/structure_optimizing.py
@@ -164,13 +164,13 @@ class gradient_descent(initialize_optimizing):
             # y(k) = -(F(k+1) - F(k))
             y_k = self.current_forces - next_forces
             # rho(k) = 1/(y(k)^T @ s(k))
-            rho_k = np.array([1/(y_k[i] @ s_k[i]) for i in self.n_atom])
+            rho_k = np.array([1/(y_k[i] @ s_k[i]) for i in range(self.n_atom)])
             # H(k+1) = (I - rho(k) s(k) y(k).T) H(k) (I - rho(k) y(k) s(k).T) + rho(k) s(k) s(k).T
             next_H = [(np.identity(3) - rho_k[i] @ np.outer(s_k[i], y_k[i])) @ self.H_k[i] @ (np.identity(3) - rho_k[i] @ np.outer(y_k[i], s_k[i]) + \
-              rho_k[i] @ np.outer(s_k[i], s_k[i])) for i in self.n_atom]
+              rho_k[i] @ np.outer(s_k[i], s_k[i])) for i in range(self.n_atom)]
 
             # p(k+1) = H(k+1) @ F(k+1)
-            self.p_k = np.array([next_H[i] @ next_forces[i] for i in self.n_atom])
+            self.p_k = np.array([next_H[i] @ next_forces[i] for i in range(self.n_atom)])
 
             # update Hessian
             self.H_k = next_H
