7ff7b9fa15c1a89432af537549b478a1494e6238
onewhich <which_1987@163.com>
2023-07-11 00:43:22 +0200

    Update structure_optimizing.py

--- src/structure_optimizing.py
+++ src/structure_optimizing.py
@@ -184,7 +184,7 @@ class gradient_descent(initialize_optimizing):
             self.log_morest.write(str(self.H_k)+'\n')
             # p(k+1) = H(k+1) @ F(k+1)
             #self.p_k = np.array([next_H[i] @ next_forces[i] for i in range(self.n_atom)])
-            self.p_k = (next_H @ next_forces.flatten()).reshape(np.shape(next_forces))
+            self.p_k = -(next_H @ next_forces.flatten()).reshape(np.shape(next_forces))
 
             # update Hessian
             self.H_k = next_H
