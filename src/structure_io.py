b15aab72e6ecf2a09ac86c7b23f8d7685258459c
onewhich <which_1987@163.com>
2024-04-06 14:44:25 +0200

    Update structure_io.py

--- src/structure_io.py
+++ src/structure_io.py
@@ -3,7 +3,7 @@ import ase.io
 
 def read_xyz_file(xyz_file):
     '''
-    Read extxyz structure file, MoREST.str or MoREST.str_new file and return a system (ase.Atoms object)
+    Read extxyz structure file and return a system (ase.Atoms object)
     '''
     system = ase.io.read(xyz_file, format='extxyz')
     
@@ -11,13 +11,13 @@ def read_xyz_file(xyz_file):
 
 def write_xyz_file(xyz_file, system):
     '''
-    Write system (ase.Atoms object) to a extxyz format file, MoREST.str_new
+    Write system (ase.Atoms object) to a extxyz format file
     '''
     ase.io.write(xyz_file, system, format='extxyz')
     
 def read_xyz_traj(traj_file):
     '''
-    Read xyz format trajectory file, MoREST.xyz_traj, and return a system_list (list of ase.Atoms objects)
+    Read xyz format trajectory file, and return a system_list (list of ase.Atoms objects)
     '''
     system_list = ase.io.read(traj_file, index=':', format='extxyz')
     
@@ -25,6 +25,6 @@ def read_xyz_traj(traj_file):
     
 def write_xyz_traj(traj_file, system_new):
     '''
-    Write the list of system (ase.Atoms object) to a extxyz format file, MoREST.xyz_traj
+    Write the list of system (ase.Atoms object) to a extxyz format file
     '''
     ase.io.write(traj_file, system_new, format='extxyz', append=True)
