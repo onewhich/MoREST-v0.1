8bb07701bf1989cb259516636e168b7a32286ae5
onewhich <which_1987@163.com>
2024-09-02 20:24:53 +0200

    Add output energy from xyz traj in structure_io

--- src/structure_io.py
+++ src/structure_io.py
@@ -1,5 +1,6 @@
 import numpy as np
 import ase.io
+from ase.units import kB
 
 def read_xyz_file(xyz_file):
     '''
@@ -28,3 +29,18 @@ def write_xyz_traj(traj_file, system_new):
     Write the list of system (ase.Atoms object) to a extxyz format file
     '''
     ase.io.write(traj_file, system_new, format='extxyz', append=True)
+
+def output_energy_from_xyz_traj(logfilename, traj_file):
+    '''
+    Output the potential energy, the kinetic energy, the temperature of the kinetic energy, the total energy
+    '''
+    logfile = open(logfilename,'w')
+    traj = read_xyz_traj(traj_file)
+    logfile.write('# RPMD step, Potential energy (eV), Kinetic energy (eV), Instant temperature (K), Total energy (eV)\n')  
+    n_atom = traj[0].get_global_number_of_atoms()
+    for i,i_sys in enumerate(traj):
+        Ep = i_sys.get_potential_energy()
+        Ek = i_sys.get_kinetic_energy()
+        T = 2/3 * Ek/kB /n_atom   # Ek = 1/2 m v^2 = 3/2 kB T for each particle
+        Et = Ek + Ep
+        logfile.write(str(i)+'    '+str(Ep)+'    '+str(Ek)+'    '+str(T)+'    '+str(Et)+'\n')
