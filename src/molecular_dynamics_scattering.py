cbcb28d9b375eb3bc2483024f20fe3e1c77377ba
onewhich <which_1987@163.com>
2025-04-06 22:55:02 +0200

    Update trajectory scattering module

---
+++ src/molecular_dynamics_scattering.py
@@ -0,0 +1,158 @@
+from structure_io import write_xyz_traj
+from trajectory_scattering import initialize_scattering
+from numerical_integraion import MD_integration
+
+class scattering_velocity_Verlet(initialize_scattering):
+    '''
+    This class implements velocity Verlet algorithm to do microcanonical ensemble (NVE) dynamics.
+    '''
+    
+    def __init__(self, morest_parameters, scattering_parameters, calculator=None, log_morest=None):
+        super(scattering_velocity_Verlet, self).__init__(morest_parameters, scattering_parameters, calculator, log_morest)
+        self.integration = MD_integration(self.many_body_potential)
+        
+    def generate_new_step(self, bias_forces=None):
+        time_step = self.scattering_parameters['scattering_time_step']
+        
+        ### F(t) + bias
+        if bias_forces != None:
+            self.current_forces = self.current_forces + bias_forces
+
+        next_potential_energy, next_forces  = self.integration.velocity_Verlet(time_step, self.current_system, self.current_forces, self.masses)
+        if self.scattering_parameters['scattering_fix_target']:
+            next_momenta = self.current_system.get_momenta()
+            next_momenta[0:self.n_atom_target] = self.clean_translation(next_momenta[0:self.n_atom_target])
+            self.current_system.set_momenta(next_momenta)
+        
+        self.current_step += 1
+        self.current_forces = next_forces
+        self.current_potential_energy = next_potential_energy
+            
+        try:
+            self.ml_calculator.get_current_step(self.current_step)
+        except:
+            pass
+        
+        if self.current_step % self.scattering_parameters['scattering_traj_interval'] == 0:
+            #self.current_traj.append(self.current_system)
+            write_xyz_traj(self.traj_filename, self.current_system)
+            kinetic_energy = self.current_system.get_kinetic_energy()
+            self.write_MD_log(self.MD_log, self.current_step, self.current_potential_energy, kinetic_energy, self.masses)
+        
+        return self.current_step, self.current_system
+
+class scattering_Runge_Kutta_4th(initialize_scattering):
+    '''
+    This class implements Runge-Kutta 4th order algorithm to do microcanonical ensemble (NVE) dynamics.
+    '''
+    
+    def __init__(self, morest_parameters, scattering_parameters, calculator=None, log_morest=None):
+        super(scattering_Runge_Kutta_4th, self).__init__(morest_parameters, scattering_parameters, calculator, log_morest)
+        self.integration = MD_integration(self.many_body_potential)
+        
+    def generate_new_step(self, bias_forces=None):
+        '''
+        This version comes from classic Runge-Kutta methods:
+        Runge–Kutta methods. (2022, September 6). In Wikipedia. https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta_methods
+         https://www.haroldserrano.com/blog/visualizing-the-runge-kutta-method
+        '''
+        time_step = self.scattering_parameters['scattering_time_step']
+        
+        ### F(t) + bias
+        if bias_forces != None:
+            self.current_forces = self.current_forces + bias_forces
+        
+        next_potential_energy, next_forces  = self.integration.Runge_Kutta_4th(time_step, self.current_system, self.current_forces, self.masses)
+        if self.scattering_parameters['scattering_fix_target']:
+            next_momenta = self.current_system.get_momenta()
+            next_momenta[0:self.n_atom_target] = self.clean_translation(next_momenta[0:self.n_atom_target])
+            self.current_system.set_momenta(next_momenta)
+        
+        self.current_step += 1
+        self.current_forces = next_forces
+        self.current_potential_energy = next_potential_energy
+            
+        try:
+            self.ml_calculator.get_current_step(self.current_step)
+        except:
+            pass
+        
+        if self.current_step % self.scattering_parameters['scattering_traj_interval'] == 0:
+            #self.current_traj.append(self.current_system)
+            write_xyz_traj(self.traj_filename, self.current_system)
+            kinetic_energy = self.current_system.get_kinetic_energy()
+            self.write_MD_log(self.MD_log, self.current_step, self.current_potential_energy, kinetic_energy, self.masses)
+        
+        return self.current_step, self.current_system
+ 
+# Discarded: the following method is not energy preseved.
+class scattering_Runge_Kutta_4th_a(initialize_scattering):
+    '''
+    This class implements Runge-Kutta 4th order algorithm to do microcanonical ensemble (NVE) dynamics.
+    '''
+    
+    def __init__(self, morest_parameters, scattering_parameters, calculator=None, log_morest=None):
+        super(scattering_Runge_Kutta_4th_a, self).__init__(morest_parameters, scattering_parameters, calculator, log_morest)
+        
+    def generate_new_step(self, bias_forces=None):
+        '''
+        This Runge-Kutta-Nyström methods version comes from https://willbeason.com/2021/06/24/introduction-to-runge-kutta-nystrom-methods/
+        '''
+        time_step = self.scattering_parameters['scattering_time_step']
+        
+        ### F(t) + bias
+        if bias_forces != None:
+            self.current_forces = self.current_forces + bias_forces
+        
+        # x_1 = x_n, v_1 = v_n, a_1 = a_n
+        x_1 = self.current_system.get_positions()
+        v_1 = self.current_system.get_velocities()
+        a_1 = self.current_forces/self.masses
+
+        # x_2 = x_n + h/2 * v_1, v_2 = v_n + h/2 * a_1, a_2 = f(x_2)
+        v_2 = v_1 + time_step/2 * a_1
+        x_2 = x_1 + time_step/2 * (v_1 + v_2)/2
+        self.current_system.set_positions(x_2)
+        Ep_2, F_2 = self.many_body_potential.get_potential_forces(self.current_system)
+        a_2 = F_2/self.masses
+
+        # x_3 = x_n + h/2 * v_2, v_3 = v_n + h/2 * a_2, a_3 = f(x_3)
+        v_3 = v_1 + time_step/2 * a_2
+        x_3 = x_1 + time_step/2 * (v_1 + v_3)/2
+        self.current_system.set_positions(x_3)
+        Ep_3, F_3 = self.many_body_potential.get_potential_forces(self.current_system)
+        a_3 = F_3/self.masses
+
+        # x_4 = x_n + h * v_3, v_4 = v_n + h * a_3, a_4 = f(x_4)
+        v_4 = v_1 + time_step * a_3
+        x_4 = x_1 + time_step * (v_1 + v_4)/2
+        self.current_system.set_positions(x_4)
+        Ep_4, F_4 = self.many_body_potential.get_potential_forces(self.current_system)
+        a_4 = F_4/self.masses
+
+        # x_n+1 = x_n + h/6 * (v_1 + 2*v_2 + 2*v_3 + v_4), v_n+1 = v_n + h/6 * (a_1 + 2*a_2 + 2*a_3 + a_4)
+        next_coordinates = x_1 + time_step/6 * (v_1 + 2*v_2 + 2*v_3 + v_4)
+        next_velocities = v_1 + time_step/6 * (a_1 + 2*a_2 + 2*a_3 + a_4)
+
+        self.current_system.set_positions(next_coordinates)
+        self.current_system.set_velocities(next_velocities)
+
+        ### F(t+dt)
+        next_potential_energy, next_forces = self.many_body_potential.get_potential_forces(self.current_system)
+        
+        self.current_step += 1
+        self.current_forces = next_forces
+        self.current_potential_energy = next_potential_energy
+            
+        try:
+            self.ml_calculator.get_current_step(self.current_step)
+        except:
+            pass
+        
+        if self.current_step % self.scattering_parameters['scattering_traj_interval'] == 0:
+            #self.current_traj.append(self.current_system)
+            write_xyz_traj(self.traj_filename, self.current_system)
+            kinetic_energy = self.current_system.get_kinetic_energy()
+            self.write_MD_log(self.MD_log, self.current_step, self.current_potential_energy, kinetic_energy, self.masses)
+        
+        return self.current_step, self.current_system
--- src/trajectory_scattering.py
+++ src/trajectory_scattering.py
@@ -3,7 +3,6 @@ import os
 import numpy as np
 from structure_io import read_xyz_file, write_xyz_file, read_xyz_traj, write_xyz_traj
 from initialize_calculator import initialize_calculator
-from numerical_integraion import MD_integration
 #from copy import deepcopy
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution, Stationary
 # Stationary and ZeroRotation from ase will not change the total kinetic energy, the vibrational energy will arise after these two processes.
@@ -40,7 +39,7 @@ class initialize_scattering(initialize_calculator):
             write_xyz_traj(self.traj_filename, self.current_system)
             self.MD_log = open(log_filename, 'w', buffering=1)
             self.MD_log.write('# MD step, Potential energy (eV), Kinetic energy (eV), Instant temperature (K), Total energy (eV)\n')   
-            write_MD_log(self.MD_log, self.current_step, self.current_potential_energy, self.current_system.get_kinetic_energy(), self.masses)
+            self.write_MD_log(self.MD_log, self.current_step, self.current_potential_energy, self.current_system.get_kinetic_energy(), self.masses)
         else:
             self.scattering_log = open('MoREST_scattering.log', 'a', buffering=1)
 
@@ -65,14 +64,14 @@ class initialize_scattering(initialize_calculator):
 
 
         # get collision velocity
-        scalar_translational_velocity = np.linalg.norm(get_translational_velocity(incident_molecule) - get_translational_velocity(target_molecule))
+        scalar_translational_velocity = np.linalg.norm(self.get_translational_velocity(incident_molecule) - self.get_translational_velocity(target_molecule))
         collision_energy = 0.5 * np.sum(incident_molecule.get_masses()) * scalar_translational_velocity**2
         
         # initialize incident and target molecules
         incident_molecule.set_velocities(self.clean_translation(incident_molecule.get_velocities()))
-        reset_mass_center(incident_molecule)
+        self.reset_mass_center(incident_molecule)
         target_molecule.set_velocities(self.clean_translation(target_molecule.get_velocities()))
-        reset_mass_center(target_molecule)
+        self.reset_mass_center(target_molecule)
         if self.scattering_parameters['scattering_clean_rotation']:
             incident_molecule.set_velocities(self.clean_rotation(incident_molecule.get_velocities(), incident_molecule.get_positions(), incident_molecule.get_masses()))
             target_molecule.set_velocities(self.clean_rotation(target_molecule.get_velocities(), target_molecule.get_positions(), target_molecule.get_masses()))
@@ -176,7 +175,7 @@ class initialize_scattering(initialize_calculator):
             write_xyz_traj(self.traj_filename, self.current_system)
             self.MD_log = open(log_filename, 'w', buffering=1)
             self.MD_log.write('# MD step, Potential energy (eV), Kinetic energy (eV), Instant temperature (K), Total energy (eV)\n')   
-            write_MD_log(self.MD_log, self.current_step, self.current_potential_energy, self.current_system.get_kinetic_energy(), self.masses)
+            self.write_MD_log(self.MD_log, self.current_step, self.current_potential_energy, self.current_system.get_kinetic_energy(), self.masses)
             
     def get_current_structure(self):
         if self.scattering_parameters['scattering_initialization']:
@@ -237,231 +236,81 @@ class initialize_scattering(initialize_calculator):
         v_tang = np.cross(rotat_vector, r_vector)
         new_velocities = v_vector - v_tang
         return new_velocities
-    
 
-class scattering_velocity_Verlet(initialize_scattering):
-    '''
-    This class implements velocity Verlet algorithm to do microcanonical ensemble (NVE) dynamics.
-    '''
-    
-    def __init__(self, morest_parameters, scattering_parameters, calculator=None, log_morest=None):
-        super(scattering_velocity_Verlet, self).__init__(morest_parameters, scattering_parameters, calculator, log_morest)
-        self.integration = MD_integration(self.many_body_potential)
-        
-    def generate_new_step(self, bias_forces=None):
-        time_step = self.scattering_parameters['scattering_time_step']
-        
-        ### F(t) + bias
-        if bias_forces != None:
-            self.current_forces = self.current_forces + bias_forces
-
-        next_potential_energy, next_forces  = self.integration.velocity_Verlet(time_step, self.current_system, self.current_forces, self.masses)
-        if self.scattering_parameters['scattering_fix_target']:
-            next_momenta = self.current_system.get_momenta()
-            next_momenta[0:self.n_atom_target] = self.clean_translation(next_momenta[0:self.n_atom_target])
-            self.current_system.set_momenta(next_momenta)
-        
-        self.current_step += 1
-        self.current_forces = next_forces
-        self.current_potential_energy = next_potential_energy
-            
-        try:
-            self.ml_calculator.get_current_step(self.current_step)
-        except:
-            pass
-        
-        if self.current_step % self.scattering_parameters['scattering_traj_interval'] == 0:
-            #self.current_traj.append(self.current_system)
-            write_xyz_traj(self.traj_filename, self.current_system)
-            kinetic_energy = self.current_system.get_kinetic_energy()
-            write_MD_log(self.MD_log, self.current_step, self.current_potential_energy, kinetic_energy, self.masses)
-        
-        return self.current_step, self.current_system
-
-class scattering_Runge_Kutta_4th(initialize_scattering):
-    '''
-    This class implements Runge-Kutta 4th order algorithm to do microcanonical ensemble (NVE) dynamics.
-    '''
-    
-    def __init__(self, morest_parameters, scattering_parameters, calculator=None, log_morest=None):
-        super(scattering_Runge_Kutta_4th, self).__init__(morest_parameters, scattering_parameters, calculator, log_morest)
-        self.integration = MD_integration(self.many_body_potential)
-        
-    def generate_new_step(self, bias_forces=None):
+    @staticmethod
+    def reset_geometric_center(system):
         '''
-        This version comes from classic Runge-Kutta methods:
-        Runge–Kutta methods. (2022, September 6). In Wikipedia. https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta_methods
-         https://www.haroldserrano.com/blog/visualizing-the-runge-kutta-method
+        set the geometric center to [0,0,0]
         '''
-        time_step = self.scattering_parameters['scattering_time_step']
-        
-        ### F(t) + bias
-        if bias_forces != None:
-            self.current_forces = self.current_forces + bias_forces
-        
-        next_potential_energy, next_forces  = self.integration.Runge_Kutta_4th(time_step, self.current_system, self.current_forces, self.masses)
-        if self.scattering_parameters['scattering_fix_target']:
-            next_momenta = self.current_system.get_momenta()
-            next_momenta[0:self.n_atom_target] = self.clean_translation(next_momenta[0:self.n_atom_target])
-            self.current_system.set_momenta(next_momenta)
-        
-        self.current_step += 1
-        self.current_forces = next_forces
-        self.current_potential_energy = next_potential_energy
-            
-        try:
-            self.ml_calculator.get_current_step(self.current_step)
-        except:
-            pass
-        
-        if self.current_step % self.scattering_parameters['scattering_traj_interval'] == 0:
-            #self.current_traj.append(self.current_system)
-            write_xyz_traj(self.traj_filename, self.current_system)
-            kinetic_energy = self.current_system.get_kinetic_energy()
-            write_MD_log(self.MD_log, self.current_step, self.current_potential_energy, kinetic_energy, self.masses)
-        
-        return self.current_step, self.current_system
- 
-# Discarded: the following method is not energy preseved.
-class scattering_Runge_Kutta_4th_a(initialize_scattering):
-    '''
-    This class implements Runge-Kutta 4th order algorithm to do microcanonical ensemble (NVE) dynamics.
-    '''
-    
-    def __init__(self, morest_parameters, scattering_parameters, calculator=None, log_morest=None):
-        super(scattering_Runge_Kutta_4th_a, self).__init__(morest_parameters, scattering_parameters, calculator, log_morest)
-        
-    def generate_new_step(self, bias_forces=None):
+        coordinates = system.get_positions()
+        n_atom = system.get_global_number_of_atoms()
+        geometric_center = np.sum(coordinates, axis=0)/n_atom
+        system.set_positions(coordinates - geometric_center)
+
+    @staticmethod
+    def reset_mass_center(system):
         '''
-        This Runge-Kutta-Nyström methods version comes from https://willbeason.com/2021/06/24/introduction-to-runge-kutta-nystrom-methods/
+        set the mass center to [0,0,0]
         '''
-        time_step = self.scattering_parameters['scattering_time_step']
-        
-        ### F(t) + bias
-        if bias_forces != None:
-            self.current_forces = self.current_forces + bias_forces
-        
-        # x_1 = x_n, v_1 = v_n, a_1 = a_n
-        x_1 = self.current_system.get_positions()
-        v_1 = self.current_system.get_velocities()
-        a_1 = self.current_forces/self.masses
-
-        # x_2 = x_n + h/2 * v_1, v_2 = v_n + h/2 * a_1, a_2 = f(x_2)
-        v_2 = v_1 + time_step/2 * a_1
-        x_2 = x_1 + time_step/2 * (v_1 + v_2)/2
-        self.current_system.set_positions(x_2)
-        Ep_2, F_2 = self.many_body_potential.get_potential_forces(self.current_system)
-        a_2 = F_2/self.masses
-
-        # x_3 = x_n + h/2 * v_2, v_3 = v_n + h/2 * a_2, a_3 = f(x_3)
-        v_3 = v_1 + time_step/2 * a_2
-        x_3 = x_1 + time_step/2 * (v_1 + v_3)/2
-        self.current_system.set_positions(x_3)
-        Ep_3, F_3 = self.many_body_potential.get_potential_forces(self.current_system)
-        a_3 = F_3/self.masses
-
-        # x_4 = x_n + h * v_3, v_4 = v_n + h * a_3, a_4 = f(x_4)
-        v_4 = v_1 + time_step * a_3
-        x_4 = x_1 + time_step * (v_1 + v_4)/2
-        self.current_system.set_positions(x_4)
-        Ep_4, F_4 = self.many_body_potential.get_potential_forces(self.current_system)
-        a_4 = F_4/self.masses
-
-        # x_n+1 = x_n + h/6 * (v_1 + 2*v_2 + 2*v_3 + v_4), v_n+1 = v_n + h/6 * (a_1 + 2*a_2 + 2*a_3 + a_4)
-        next_coordinates = x_1 + time_step/6 * (v_1 + 2*v_2 + 2*v_3 + v_4)
-        next_velocities = v_1 + time_step/6 * (a_1 + 2*a_2 + 2*a_3 + a_4)
-
-        self.current_system.set_positions(next_coordinates)
-        self.current_system.set_velocities(next_velocities)
-
-        ### F(t+dt)
-        next_potential_energy, next_forces = self.many_body_potential.get_potential_forces(self.current_system)
-        
-        self.current_step += 1
-        self.current_forces = next_forces
-        self.current_potential_energy = next_potential_energy
-            
-        try:
-            self.ml_calculator.get_current_step(self.current_step)
-        except:
-            pass
-        
-        if self.current_step % self.scattering_parameters['scattering_traj_interval'] == 0:
-            #self.current_traj.append(self.current_system)
-            write_xyz_traj(self.traj_filename, self.current_system)
-            kinetic_energy = self.current_system.get_kinetic_energy()
-            write_MD_log(self.MD_log, self.current_step, self.current_potential_energy, kinetic_energy, self.masses)
-        
-        return self.current_step, self.current_system
-
-def reset_geometric_center(system):
-    '''
-    set the geometric center to [0,0,0]
-    '''
-    coordinates = system.get_positions()
-    n_atom = system.get_global_number_of_atoms()
-    geometric_center = np.sum(coordinates, axis=0)/n_atom
-    system.set_positions(coordinates - geometric_center)
-
-def reset_mass_center(system):
-    '''
-    set the mass center to [0,0,0]
-    '''
-    coordinates = system.get_positions()
-    masses = system.get_masses()[:,np.newaxis]
-    mass_center = np.sum(masses*coordinates, axis=0)/np.sum(masses)
-    system.set_positions(coordinates - mass_center)
-
-def get_translational_momentum(system):
-    n_atom = system.get_global_number_of_atoms()
-    return np.sum(system.get_momenta(), axis=0)/n_atom
-
-def get_translational_velocity(system):
-    n_atom = system.get_global_number_of_atoms()
-    return np.sum(system.get_velocities(),axis=0)/n_atom
-
-def rotate_system_at_center(system, theta, unit_normal_vector, center=[0,0,0]):
-    '''
-    theta: angles, in degree
-    center: 1-D list of 3 numbers (define the position of the center), or 'geometry' (calculate geometric center), or 'mass' (calculate mass' center)
-    r x r' = n,
-    when r and n are orthogonal, r' = (n x r) / (|r|*|n|) * r +  cos(theta) * r
-    '''
-    # (r x r_new) / (|r| * |r_new|) = sin(theta) * unit_normal_vector, where |r| == |r_new|
-    # r x r_new = sin(theta) * unit_normal_vector * |r|^2
-    theta = np.deg2rad(theta)
-    unit_normal_vector = np.array(unit_normal_vector)
-    unit_normal_vector = unit_normal_vector / np.linalg.norm(unit_normal_vector)
-    coordinates = system.get_positions()
-    if type(center) == list:
-        center = np.array(center)
-    elif center.upper() == 'geometry'.upper():
-        center = np.sum(coordinates, axis=0)/len(coordinates)
-    elif center.upper() == 'mass'.upper():
+        coordinates = system.get_positions()
         masses = system.get_masses()[:,np.newaxis]
-        center = np.sum(masses*coordinates, axis=0)/np.sum(masses)
-    coordinates = coordinates - center
+        mass_center = np.sum(masses*coordinates, axis=0)/np.sum(masses)
+        system.set_positions(coordinates - mass_center)
 
-    r_cross_r_new = np.sin(theta) * unit_normal_vector
-    r = np.linalg.norm(coordinates, axis=1)[:,np.newaxis]
-    r2 = r**2
-    r_cross_r_new = r_cross_r_new * r2
+    @staticmethod
+    def get_translational_momentum(system):
+        n_atom = system.get_global_number_of_atoms()
+        return np.sum(system.get_momenta(), axis=0)/n_atom
 
-    system_new = system.copy()
-    coordinates_new = np.cross(r_cross_r_new, coordinates) / r2 + np.cos(theta) * coordinates
-    system_new.set_positions(coordinates_new)
+    @staticmethod
+    def get_translational_velocity(system):
+        n_atom = system.get_global_number_of_atoms()
+        return np.sum(system.get_velocities(),axis=0)/n_atom
 
-    return system_new
+    @staticmethod
+    def rotate_system_at_center(system, theta, unit_normal_vector, center=[0,0,0]):
+        '''
+        theta: angles, in degree
+        center: 1-D list of 3 numbers (define the position of the center), or 'geometry' (calculate geometric center), or 'mass' (calculate mass' center)
+        r x r' = n,
+        when r and n are orthogonal, r' = (n x r) / (|r|*|n|) * r +  cos(theta) * r
+        '''
+        # (r x r_new) / (|r| * |r_new|) = sin(theta) * unit_normal_vector, where |r| == |r_new|
+        # r x r_new = sin(theta) * unit_normal_vector * |r|^2
+        theta = np.deg2rad(theta)
+        unit_normal_vector = np.array(unit_normal_vector)
+        unit_normal_vector = unit_normal_vector / np.linalg.norm(unit_normal_vector)
+        coordinates = system.get_positions()
+        if type(center) == list:
+            center = np.array(center)
+        elif center.upper() == 'geometry'.upper():
+            center = np.sum(coordinates, axis=0)/len(coordinates)
+        elif center.upper() == 'mass'.upper():
+            masses = system.get_masses()[:,np.newaxis]
+            center = np.sum(masses*coordinates, axis=0)/np.sum(masses)
+        coordinates = coordinates - center
 
-def write_MD_log(MD_log, step, Ep, Ek, masses):
-    try:
-        if len(Ep) >= 1:
-            Ep = Ep[0]
-    except:
-        pass
-    n_atom = len(masses)
-    #Ek = np.sum([0.5 * masses[i] * np.linalg.norm(velocities[i])**2 for i in range(n_atom)])
-    #Ek = np.sum(0.5 * masses * np.linalg.norm(velocities)**2)
-    T = 2/3 * Ek/units.kB /n_atom   # Ek = 1/2 m v^2 = 3/2 kB T for each particle
-    Et = Ek + Ep
-    MD_log.write(str(step)+'    '+str(Ep)+'    '+str(Ek)+'    '+str(T)+'    '+str(Et)+'\n')
+        r_cross_r_new = np.sin(theta) * unit_normal_vector
+        r = np.linalg.norm(coordinates, axis=1)[:,np.newaxis]
+        r2 = r**2
+        r_cross_r_new = r_cross_r_new * r2
+
+        system_new = system.copy()
+        coordinates_new = np.cross(r_cross_r_new, coordinates) / r2 + np.cos(theta) * coordinates
+        system_new.set_positions(coordinates_new)
+
+        return system_new
+
+    @staticmethod
+    def write_MD_log(MD_log, step, Ep, Ek, masses):
+        try:
+            if len(Ep) >= 1:
+                Ep = Ep[0]
+        except:
+            pass
+        n_atom = len(masses)
+        #Ek = np.sum([0.5 * masses[i] * np.linalg.norm(velocities[i])**2 for i in range(n_atom)])
+        #Ek = np.sum(0.5 * masses * np.linalg.norm(velocities)**2)
+        T = 2/3 * Ek/units.kB /n_atom   # Ek = 1/2 m v^2 = 3/2 kB T for each particle
+        Et = Ek + Ep
+        MD_log.write(str(step)+'    '+str(Ep)+'    '+str(Ek)+'    '+str(T)+'    '+str(Et)+'\n')
