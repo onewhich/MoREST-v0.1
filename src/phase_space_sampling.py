84805b1faf15a3994a634737a258a0d439935772
onewhich <which_1987@163.com>
2025-04-20 00:29:21 +0200

    Update phase_space_sampling.py

--- src/phase_space_sampling.py
+++ src/phase_space_sampling.py
@@ -141,7 +141,7 @@ class MD(initialize_sampling):
                 elif 'sampling_initial_T' in self.sampling_parameters:
                     T_thermalized = self.sampling_parameters['sampling_initial_T']
                     MaxwellBoltzmannDistribution(self.current_system, temperature_K = T_thermalized, force_temp = True)
-                elif self.T_simulation > 1e-3:
+                else:
                     MaxwellBoltzmannDistribution(self.current_system, temperature_K = self.T_simulation)
             
             #self.current_traj = []
@@ -238,15 +238,15 @@ class RPMD(initialize_sampling):
             if not self.sampling_parameters['sampling_pre_thermalized']:
                 if 'sampling_initial_E' in self.sampling_parameters:
                     T_thermalized = 2/3 * self.sampling_parameters['sampling_initial_E']/units.kB /self.n_atom   # Ek = 1/2 m v^2 = 3/2 kB T for each particle
-                    for i in range(self.n_beads):
-                        MaxwellBoltzmannDistribution(self.current_beads[i], temperature_K = T_thermalized, force_temp = True)
+                    MaxwellBoltzmannDistribution(self.current_system, temperature_K = T_thermalized, force_temp = True)
                 elif 'sampling_initial_T' in self.sampling_parameters:
                     T_thermalized = self.sampling_parameters['sampling_initial_T']
-                    for i in range(self.n_beads):
-                        MaxwellBoltzmannDistribution(self.current_beads[i], temperature_K = T_thermalized, force_temp = True)
-                elif self.T_simulation > 1e-3:
-                    for i in range(self.n_beads):
-                        MaxwellBoltzmannDistribution(self.current_beads[i], temperature_K = self.T_simulation)
+                    MaxwellBoltzmannDistribution(self.current_system, temperature_K = T_thermalized, force_temp = True)
+                else:
+                    MaxwellBoltzmannDistribution(self.current_system, temperature_K = self.T_simulation)
+                total_momenta = self.current_system.get_momenta()
+                for i in range(self.n_beads):
+                    self.current_beads[i].set_momenta(total_momenta)
 
         self.current_beads_positions = self.get_beads_positions(self.current_beads)
         self.current_beads_momenta = self.get_beads_momenta(self.current_beads)
