24039fc06b4f96be0c7638417db6949914b3ef64
onewhich <onewhichy@gmail.com>
2025-12-01 17:36:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="MoREST",
     version="0.1",
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     install_requires=["numpy","scipy","ase","tqdm"],
 )
